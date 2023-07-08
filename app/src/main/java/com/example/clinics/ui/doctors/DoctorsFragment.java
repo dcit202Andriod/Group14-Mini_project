9bc51275cbfad839da862e1c2ff91bd47dbcb4ed
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-08 17:09:02 +0000

    added doctor views

--- app/src/main/java/com/example/clinics/ui/doctors/DoctorsFragment.java
+++ app/src/main/java/com/example/clinics/ui/doctors/DoctorsFragment.java
@@ -4,34 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.clinics.databinding.FragmentServiceBinding;
+import com.example.clinics.R;
 
 public class DoctorsFragment extends Fragment {
 
-    private FragmentServiceBinding binding;
+    private ListView doctorsListView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DoctorsViewModel serviceViewModel =
-                new ViewModelProvider(this).get(DoctorsViewModel.class);
-
-        binding = FragmentServiceBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textGallery;
-        serviceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
+    @Nullable
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_doctors, container, false);
+        doctorsListView = rootView.findViewById(R.id.doctorsListView);
+
+        // Sample data of doctors and their positions
+        String[] doctors = {
+                "Dr. John Smith - Cardiologist",
+                "Dr. Jane Doe - Neurologist",
+                "Dr. Robert Johnson - Orthopedic Surgeon",
+                "Dr. Sarah Williams - Pediatrician",
+                "Dr. Michael Brown - Dermatologist"
+        };
+
+        // Create an ArrayAdapter to populate the ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, doctors);
+        doctorsListView.setAdapter(adapter);
+
+        return rootView;
     }
 }
