54e3e6119b112528a49627678c740df51a56c5ea
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-08 22:27:26 +0000

    added: java file fragment for doctors

--- app/src/main/java/com/example/clinics/ui/doctors/DoctorsFragment.java
+++ app/src/main/java/com/example/clinics/ui/doctors/DoctorsFragment.java
@@ -4,28 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.clinics.databinding.FragmentServiceBinding;
+import com.example.clinics.databinding.FragmentDoctorsBinding;
 
 public class DoctorsFragment extends Fragment {
 
-    private FragmentServiceBinding binding;
+    private FragmentDoctorsBinding binding;
+    private DoctorsAdapter doctorsAdapter;
+    private DoctorsViewModel doctorsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DoctorsViewModel serviceViewModel =
-                new ViewModelProvider(this).get(DoctorsViewModel.class);
-
-        binding = FragmentServiceBinding.inflate(inflater, container, false);
+        binding = FragmentDoctorsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        serviceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        doctorsViewModel = new ViewModelProvider(this).get(DoctorsViewModel.class);
+        doctorsViewModel.getDoctors().observe(getViewLifecycleOwner(), doctors -> {
+            doctorsAdapter = new DoctorsAdapter(requireContext(), doctors);
+            binding.listViewDoctors.setAdapter(doctorsAdapter);
+        });
+
         return root;
     }
 
