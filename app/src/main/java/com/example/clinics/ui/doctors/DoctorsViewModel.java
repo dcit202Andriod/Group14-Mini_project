0da2ca2d89161666f85775810de39f2053b06100
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-09 01:59:11 +0000

    changes made to doctors

--- app/src/main/java/com/example/clinics/ui/doctors/DoctorsViewModel.java
+++ app/src/main/java/com/example/clinics/ui/doctors/DoctorsViewModel.java
@@ -22,9 +22,16 @@ public class DoctorsViewModel extends ViewModel {
 
     private List<Doctor> getDoctorsList() {
         List<Doctor> doctorsList = new ArrayList<>();
-        doctorsList.add(new Doctor("Dr. John Doe", "Cardiology"));
-        doctorsList.add(new Doctor("Dr. Jane Smith", "Dermatology"));
+        doctorsList.add(new Doctor("Dr. Daniel Rodriguez", "Head of Internal Medicine"));
+        doctorsList.add(new Doctor("Dr. Benjamin Cooper", "Director of Anesthesiology"));
         doctorsList.add(new Doctor("Dr. Alex Johnson", "Orthopedics"));
+        doctorsList.add(new Doctor("Dr. Olivia Hall", "Chief of Endocrinology"));
+        doctorsList.add(new Doctor("Dr. Megan Thompson", "Director of Psychiatry"));
+        doctorsList.add(new Doctor("Dr. Amanda Lewis", "Director of Pulmonology"));
+        doctorsList.add(new Doctor("Dr. Jennifer Lee", "Director of Emergency Medicine"));
+        doctorsList.add(new Doctor("Dr. Michael Johnson", "Director of Surgery"));
+        doctorsList.add(new Doctor("Dr. Robert Wilson", "Head of Neurology"));
+        doctorsList.add(new Doctor("Dr. David Anderson", "Chief of Cardiology"));
         return doctorsList;
     }
 }
