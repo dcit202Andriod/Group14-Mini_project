e99f5bc475e7e2188ab16695e2ce65694e8b863b
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-09 13:23:52 +0000

    modified: AppointmentFragment file

--- app/src/main/java/com/example/clinics/ui/appointment/AppointmentFragment.java
+++ app/src/main/java/com/example/clinics/ui/appointment/AppointmentFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,11 +18,12 @@ import com.example.clinics.R;
 
 public class AppointmentFragment extends Fragment {
 
-    private EditText doctorNameEditText;
+    private EditText patientNameEditText;
     private EditText timeEditText;
     private EditText dayEditText;
     private EditText emailEditText;
     private AppointmentViewModel appointmentViewModel;
+    private Spinner spinner;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +36,19 @@ public class AppointmentFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_appointment, container, false);
 
         // Initialize the UI components
-        doctorNameEditText = view.findViewById(R.id.doctorNameEditText);
+        spinner = view.findViewById(R.id.spinner);
+
+        // Create an ArrayAdapter using a string array resource and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        patientNameEditText = view.findViewById(R.id.patient_name);
         timeEditText = view.findViewById(R.id.timeEditText);
         dayEditText = view.findViewById(R.id.dayEditText);
         emailEditText = view.findViewById(R.id.emailEditText);
@@ -41,28 +56,23 @@ public class AppointmentFragment extends Fragment {
 
         submitButton.setOnClickListener(v -> {
             // Retrieve the input values
-            String doctorName = doctorNameEditText.getText().toString();
+            String patientName = patientNameEditText.getText().toString();
             String time = timeEditText.getText().toString();
             String day = dayEditText.getText().toString();
             String email = emailEditText.getText().toString();
 
             // Pass the data to the ViewModel
-            appointmentViewModel.submitAppointment(doctorName, time, day, email);
+            appointmentViewModel.submitAppointment(patientName, time, day, email);
 
-            if (doctorNameEditText.getText().toString().equals("") ||
-                    timeEditText.getText().toString().equals("") ||
-                    dayEditText.getText().toString().equals("") ||
-                    emailEditText.getText().toString().equals("")) {
-
-                Toast.makeText(getActivity().getApplicationContext(), "All fields are required!!", Toast.LENGTH_SHORT).show();
+            if (patientName.isEmpty() || time.isEmpty() || day.isEmpty() || email.isEmpty()) {
+                Toast.makeText(requireContext(), "All fields are required!", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(getActivity().getApplicationContext(), "Appointment submitted successfully", Toast.LENGTH_SHORT).show();
-                doctorNameEditText.setText("");
+                Toast.makeText(requireContext(), "Appointment submitted successfully", Toast.LENGTH_SHORT).show();
+                patientNameEditText.setText("");
                 timeEditText.setText("");
                 dayEditText.setText("");
                 emailEditText.setText("");
             }
-
         });
 
         return view;
