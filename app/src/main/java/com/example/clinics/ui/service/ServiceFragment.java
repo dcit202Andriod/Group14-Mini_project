f7c887bf8962e7bd6fed50d269a74a7e3aff30d6
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-10 21:11:09 +0000

    edited: service.xml

--- app/src/main/java/com/example/clinics/ui/service/ServiceFragment.java
+++ app/src/main/java/com/example/clinics/ui/service/ServiceFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.clinics.databinding.FragmentServiceBinding;
@@ -15,26 +16,48 @@ import com.example.clinics.databinding.FragmentServiceBinding;
 public class ServiceFragment extends Fragment {
 
     private FragmentServiceBinding binding;
+    private ServiceViewModel serviceViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ServiceViewModel serviceViewModel =
-                new ViewModelProvider(this).get(ServiceViewModel.class);
-
         binding = FragmentServiceBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.serviceTextView;
-        serviceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView serviceTextView = binding.serviceText;
+        final TextView emergencyServiceTextView = binding.emergencyServiceText;
+        final TextView laboratoryServiceTextView = binding.laboratoryServiceText;
+        final TextView radiologyServiceTextView = binding.radiologyServiceText;
 
-        final TextView emergencyServiceTextView = binding.emergencyServiceTextView;
-        serviceViewModel.getEmergencyService().observe(getViewLifecycleOwner(), emergencyServiceTextView::setText);
 
-        final TextView laboratoryServiceTextView = binding.laboratoryServiceTextView;
-        serviceViewModel.getLaboratoryService().observe(getViewLifecycleOwner(), laboratoryServiceTextView::setText);
+        serviceViewModel = new ViewModelProvider(this).get(ServiceViewModel.class);
 
-        final TextView radiologyServiceTextView = binding.radiologyServiceTextView;
-        serviceViewModel.getRadiologyService().observe(getViewLifecycleOwner(), radiologyServiceTextView::setText);
+        serviceViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String text) {
+                serviceTextView.setText(text);
+            }
+        });
+
+        serviceViewModel.getEmergencyService().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String emergencyService) {
+                emergencyServiceTextView.setText(emergencyService);
+            }
+        });
+
+        serviceViewModel.getLaboratoryService().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String laboratoryService) {
+                laboratoryServiceTextView.setText(laboratoryService);
+            }
+        });
+
+        serviceViewModel.getRadiologyService().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String radiologyService) {
+                radiologyServiceTextView.setText(radiologyService);
+            }
+        });
 
         return root;
     }
